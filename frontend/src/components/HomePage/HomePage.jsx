e775280e8025f75207df8eb5dcf76d281fb7fe78
vishnuvr16 <vishnuvardhanreddygajjala1@gmail.com>
2024-07-18 11:46:44 +0530

    changed some styles

--- frontend/src/components/HomePage/HomePage.jsx
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -130,7 +130,7 @@ const HomePage = () => {
               key to our success.
             </p>
             <a
-              className="inline-flex items-center justify-center px-7 py-3 h-14 w-full md:w-auto text-lg leading-7 text-green-50 bg-green-500 hover:bg-green-600 font-medium focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 border border-transparent rounded-md shadow-sm"
+              className="inline-flex items-center justify-center px-7 py-3 mb-4 h-14 w-full md:w-auto text-lg leading-7 text-green-50 bg-green-500 hover:bg-green-600 font-medium focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 border border-transparent rounded-md shadow-sm"
               href="#"
             >
               Get Started
